82200ea68d67c0068c20f1c166ed6e37fc13e68b
javalsai <jvssxxi@gmail.com>
2024-06-09 12:47:56 +0200

    feat: implement chacha20

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,32 @@
 use wasm_bindgen::prelude::*;
+use chacha20::{
+    cipher::{Iv, KeyIvInit, StreamCipher},
+    ChaCha20, Key, Nonce,
+};
 
 #[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
+pub struct Cipher {
+    cipher: ChaCha20,
 }
 
+// rust basics
 #[wasm_bindgen]
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+impl Cipher {
+    #[wasm_bindgen(constructor)]
+    pub fn new(key: &[u8], nonce: &[u8]) -> Self {
+        let key: &Key = Key::from_slice(key);
+        let nonce: &Nonce = Iv::<ChaCha20>::from_slice(nonce);
+
+        let cipher = ChaCha20::new(key, nonce);
+        Self { cipher }
+    }
+}
+
+// node:streams.Transformer
+// hopefully, implementing nodejs transformer methods will allow it to behave as such
+#[wasm_bindgen]
+impl Cipher {
+    pub fn transform(&mut self, data: &mut [u8]) {
+        self.cipher.apply_keystream(data);
+    }
 }
